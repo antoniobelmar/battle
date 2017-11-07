4c8b6be7d53b4250ebd7e6e1dd8408c160184107
Joe Cowton <joecowton@gmail.com>
2017-11-07 16:12:29 +0000

    inital commit

---
+++ app.rb
@@ -0,0 +1,12 @@
+# my_app.rb
+require 'sinatra/base'
+
+class App < Sinatra::Base
+  # ... app code here ...
+
+  get '/' do
+    "Hello Battle!"
+  end
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
